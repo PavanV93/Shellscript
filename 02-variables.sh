38b43f3596c2994f4af7e678e739bd65f9717a87
pavan <vpavan7477@gmail.com>
2024-07-09 15:36:15 +0530

    hello script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-PERSON1=pavan #n space between = and vallue
+#Declaration Varaibale
+PERSON1=pavan #no space between = and vallue
 PERSON2=suresh
 
+#referring Variable
 echo "$PERSON1:: Hello $PERSON2"
 echo "$PERSON2:: Hi Hello $PERSON1"
 
---
+++ 03-Variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Declaration Varaibale
+PERSON1=$1
+PERSON2=$2
+
+#referring Variable
+echo "$PERSON1:: Hello $PERSON2"
+echo "$PERSON2:: Hi Hello $PERSON1"
